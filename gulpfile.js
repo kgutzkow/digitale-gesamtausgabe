41894c03d5b5d4818a002ec955cad851bd4f9495
Mark <Mark.Hall@work.room3b.eu>
2018-05-02 13:26:02 +0200

    Use the digital-edition-reader for reading

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
     pump = require('pump');
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css', 'js']);
 
-gulp.task('css', function(cb) {
+gulp.task('css', ['css:theme', 'css:reader']);
+
+gulp.task('css:theme', function(cb) {
     pump([
         gulp.src('theme/src/app.scss'),
         sass({
@@ -21,6 +24,25 @@ gulp.task('css', function(cb) {
     ], cb);
 });
 
+gulp.task('css:reader', function(cb) {
+    pump([
+        gulp.src(['node_modules/digital-edition-reader/dist/edition-reader.css']),
+        concat('digital-edition-reader.css'),
+        gulp.dest('theme/static/css')
+    ], cb);
+})
+
+gulp.task('js', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/digital-edition-reader/dist/edition-reader-vendor.js',
+            'node_modules/digital-edition-reader/dist/edition-reader.js',
+        ]),
+        concat('digital-edition-reader.js'),
+        gulp.dest('theme/static/js')
+    ], cb);
+});
+
 gulp.task('watch', ['default'], function(db) {
-    gulp.watch('theme/src/**/*.scss', ['css']);
+    gulp.watch('theme/src/**/*.scss', ['css:theme']);
 })
