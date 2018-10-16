055790ffaa8cd0345786202efea91aa1d23949d9
Mark Hall <mark.hall@informatik.uni-halle.de>
2018-10-16 14:44:23 +0200

    Change title. Fixes #3.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from datetime import datetime
 
 AUTHOR = 'Mark M Hall'
-SITENAME = 'Karl Gutzkow - Digitale Gesamtausgabe'
+SITENAME = 'Karl Gutzkow - Editionsprojekt'
 SITEURL = ''
 
 PATH = 'content'
