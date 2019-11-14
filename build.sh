46a6e95e388cea898e6e522996df1ee5a9f50e67
root <root@gutzkow>
2019-11-14 13:19:41 +0100

    jr: yarn setup proxy

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export https_proxy="http://192.168.5.200:3128"
 
 git pull --rebase
 export PIPENV_VENV_IN_PROJECT=True
+yarn config set proxy http://192.168.5.200:3128
+yarn config set https-proxy http://192.168.5.200:3128
 pipenv install
 yarn install
 node_modules/.bin/gulp
