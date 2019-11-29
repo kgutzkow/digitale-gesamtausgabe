f41b52749843199accbf6686ea1bec2368a9fc27
Mark Hall <mark.hall@work.room3b.eu>
2019-11-29 10:04:53 +0000

    Re-enable build

--- build.sh
+++ build.sh
@@ -40,8 +40,7 @@ export PIPENV_VENV_IN_PROJECT=True
 pipenv install
 yarn install
 node_modules/.bin/gulp
-rm -rf output
-#pipenv run pelican -o output -d content
+pipenv run pelican -o output -d content
 
 # Build the branch-specific preview sites
 #if [ -f 'branches.txt' ]
