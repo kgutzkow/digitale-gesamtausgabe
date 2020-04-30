7fbeee845bb4375c893990b64dce8f885cfe70e3
Mark Hall <mark.hall@open.ac.uk>
2020-04-30 10:04:15 +0200

    Merge Rezensionen and CI Update

--- extra_filters.py
+++ extra_filters.py
@@ -1,6 +1,7 @@
 import json
 
 from hashlib import sha256
+from jinja2.filters import environmentfilter, make_attrgetter
 from pelican import signals
 
 
@@ -10,10 +11,32 @@ def hash_sha256(text):
     return digest.hexdigest()
 
 
+@environmentfilter
+def sgroupby(environment, items, attribute):
+    """Group-by filter that assumes that the items are already sorted."""
+    attrgetter = make_attrgetter(environment, attribute)
+    grouper = None
+    tmp = []
+    result = []
+    for item in items:
+        key = attrgetter(item)
+        if key != grouper:
+            if tmp:
+                result.append((grouper, tmp))
+            grouper = key
+            tmp = [item]
+        else:
+            tmp.append(item)
+    if tmp:
+        result.append((grouper, tmp))
+    return result
+
+
 def install_extra_filters(generator):
     """Install the extra filters."""
     generator.env.filters['json'] = json.dumps
     generator.env.filters['sha256'] = hash_sha256
+    generator.env.filters['sgroupby'] = sgroupby
 
 
 def register():
