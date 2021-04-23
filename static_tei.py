b0d4b27daa9312dde3f19b0bf6951505ce98d697
Mark Hall <mark.hall@work.room3b.eu>
2021-04-23 16:12:29 +0100

    f-string fix

--- static_tei.py
+++ static_tei.py
@@ -6,7 +6,7 @@ from pelican import signals
 def write_tei(generator, content):
     """Copy the source TEI to the output path."""
     if content.source_path.endswith('.tei'):
-        output_filename = os.path.join(generator.output_path, f'{content.save_as[:-5]}.tei')
+        output_filename = os.path.join(generator.output_path, content.save_as[:-5] + '.tei')
         os.makedirs(os.path.dirname(output_filename), exist_ok=True)
         with open(content.source_path) as in_f, open(output_filename, 'w') as out_f:
             out_f.write(in_f.read())
