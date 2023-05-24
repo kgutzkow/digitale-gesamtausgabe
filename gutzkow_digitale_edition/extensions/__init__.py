f47371f9be4c4c4d1aafea88d67cf739f6ca0220
Mark Hall <mark.hall@work.room3b.eu>
2023-05-24 23:36:13 +0100

    Cleaned up and documented the extension

--- gutzkow_digitale_edition/extensions/__init__.py
+++ gutzkow_digitale_edition/extensions/__init__.py
@@ -1,13 +1,10 @@
 from sphinx.application import Sphinx
 
-from .parser import TEIParser
-from . import html, config
+from . import config, tei
 
 
 def setup(app: Sphinx):
     """Setup the theme extensions."""
     config.setup(app)
-    app.add_source_suffix('.tei', 'tei')
-    app.add_source_parser(TEIParser)
-    html.setup(app)
+    tei.setup(app)
     return {'parallel_read_safe': True, 'parallel_write_safe': True}
--- gutzkow_digitale_edition/extensions/config.py
+++ gutzkow_digitale_edition/extensions/config.py
@@ -1,42 +1,53 @@
+"""uEdition configuration handling.
+
+This module handles reading the uEdition-specific configuration settings, validating them and
+adding any required default values.
+"""
 from pydantic import BaseModel, validator, ValidationError
 from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.util import logging
-from typing import Union, Literal
+from typing import Union, Literal, Optional
 
 
 logger = logging.getLogger(__name__)
 
 
 class RuleSelectorAttribute(BaseModel):
+    """Validation rule for selecting based on an attribute with a given value."""
 
     attr: str
     value: str
 
 
 class RuleSelector(BaseModel):
+    """Validation rule for the selector for matching a TEI tag."""
 
     tag: str
     attributes: list[RuleSelectorAttribute] = []
 
     @validator('tag', pre=True)
     def expand_tag_namespace(cls, v, values, **kwargs) -> str:
+        """Expand any ```tei:``` namespace prefixes. """
         return v.replace('tei:', '{http://www.tei-c.org/ns/1.0}')
 
     @validator('attributes', pre=True)
     def convert_dict_attributes_to_list(cls, v, values, **kwargs) -> list[dict]:
+        """Convert a single attributes dictionary to a list with that dictionary."""
         if isinstance(v, dict):
             return [v]
         return v
 
 
 class RuleText(BaseModel):
+    """Validation rule for retrieving the text content from an attribute."""
 
     action: Literal['from-attribute']
     attr: str
 
 
 class RuleAttributeCopy(BaseModel):
+    """Validation rule for copying and attribute."""
 
     action: Literal['copy'] = 'copy'
     attr: str
@@ -44,6 +55,7 @@ class RuleAttributeCopy(BaseModel):
 
 
 class RuleAttributeSet(BaseModel):
+    """Validation rule for settings an attribute to a specific value."""
 
     action: Literal['set']
     attr: str
@@ -51,12 +63,14 @@ class RuleAttributeSet(BaseModel):
 
 
 class RuleAttributeDelete(BaseModel):
+    """Validation rule for deleting an attribute."""
 
     action: Literal['delete']
     attr: str
 
 
 class Rule(BaseModel):
+    """Validation model for a rule transforming a TEI tag into a HTML tag."""
 
     selector: RuleSelector
     tag: Union[str, None] = 'div'
@@ -71,17 +85,25 @@ class Rule(BaseModel):
 
 
 class Section(BaseModel):
+    """Validation model for a TEI section."""
 
     title: str
     content: str
     mappings: list[Rule] = []
 
 
-class Config(BaseModel):
+class TEIConfig(BaseModel):
+    """Validation model for the TEI-specific settings."""
 
     text_only_in_leaf_nodes: bool = False
     mappings: list[Rule] = []
     sections: list[Section] = []
+
+
+class Config(BaseModel):
+    """Configuration validation model."""
+
+    tei: Optional[TEIConfig]
 
 
 BASE_RULES = [
@@ -108,16 +130,19 @@ BASE_RULES = [
     {'selector': 'tei:lem', 'tag': 'span'},
     {'selector': 'tei:sic', 'tag': 'span'},
 ]
+"""Base mapping rules for mapping TEI tags to default HTML elements."""
 
 
 def validate_config(app: Sphinx, config: Config) -> None:
-    if 'sections' in config.uEdition and isinstance(config.uEdition['sections'], list):
-        if 'mappings' in config.uEdition and isinstance(config.uEdition['mappings'], list):
-            for section in config.uEdition['sections']:
-                if 'mappings' in section and isinstance(section['mappings'], list):
-                    section['mappings'] = section['mappings'] + config.uEdition['mappings'] + BASE_RULES
-                else:
-                    section['mappings'] = config.uEdition['mappings'] + BASE_RULES
+    """Validate the configuration and add any default values."""
+    if 'tei' in config.uEdition:
+        if 'sections' in config.uEdition['tei'] and isinstance(config.uEdition['tei']['sections'], list):
+            if 'mappings' in config.uEdition['tei'] and isinstance(config.uEdition['tei']['mappings'], list):
+                for section in config.uEdition['tei']['sections']:
+                    if 'mappings' in section and isinstance(section['mappings'], list):
+                        section['mappings'] = section['mappings'] + config.uEdition['tei']['mappings'] + BASE_RULES
+                    else:
+                        section['mappings'] = config.uEdition['tei']['mappings'] + BASE_RULES
     try:
         config.uEdition = Config(**config.uEdition).dict()
     except ValidationError as e:
@@ -128,5 +153,6 @@ def validate_config(app: Sphinx, config: Config) -> None:
 
 
 def setup(app: Sphinx) -> None:
+    """Setup the Sphinx configuration handling for the uEdition."""
     app.add_config_value('uEdition', default=None, rebuild='html', types=[dict])
     app.connect('config-inited', validate_config)
