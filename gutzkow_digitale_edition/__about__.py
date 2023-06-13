6c91dada14eea90ad742af7089ea138d8dcc2451
Mark Hall <mark.hall@work.room3b.eu>
2023-06-13 22:41:30 +0100

    Switch to using the uEdition directly

--- gutzkow_digitale_edition/__about__.py
+++
@@ -1,2 +0,0 @@
-"""About this project"""
-__version__ = '2.0.0'
--- gutzkow_digitale_edition/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Gutzkow Digitale Gesamtausgabe Extensions."""
-from sphinx.application import Sphinx
-
-from . import extensions
-
-
-def setup(app: Sphinx):
-    """Setup the theme and its extensions."""
-    extensions.setup(app)
